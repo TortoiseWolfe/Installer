e1e37eaf0ddd59b2f3e922d81e86460f24c83e47
Turtle Wolfe <JonPohlner@gmail.com>
2023-12-05 10:41:43 -0500

    kata two

---
+++ Installation_001/dojoStart.cmd
@@ -0,0 +1,4 @@
+msiexec /i C:\ProgramData\Autodesk\Revit\Addins\2023\Installation_001\dojo2.msi /l*v install.log
+
+start C:\ProgramData\Autodesk\Revit\Addins\2023\Installation_001
+start appwiz.cpl
